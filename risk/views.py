9aa608bf9b488cd3e77ced7b6da0383a52e98566
chapkovski <chapkovski@gmail.com>
2017-06-01 17:21:58 -0400

    more simple solution

--- risk/views.py
+++ risk/views.py
@@ -2,26 +2,15 @@ from otree.api import Currency as c, currency_range
 from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
-from  django.template import Context,Template
 
-
-from django import forms
-
-class NameForm(forms.Form):
-    # your_name = forms.CharField(label='Your name', max_length=100)
-    def __init__(self, fields_to_add, *args, **kwargs):
-        super(NameForm, self).__init__(*args, **kwargs)
-        CHOICES = (("True", True), ("False",False) )
-
-        for f in fields_to_add:
-            self.fields[f] = forms.CharField(widget=forms.Select(attrs={'class':'selector', }, choices=CHOICES),
-            )
 
 class Decisions(Page):
     form_model = models.Player
     form_fields = ['gamble_{}'.format(i) for i in range(1, 11)]
 
     def vars_for_template(self):
+        # a = Variable('form').resolve(self.context)
+        smth = self.get_form()
         fields_to_show=self.form_fields
         leftcol = []
         rightcol = []
@@ -31,18 +20,8 @@ class Decisions(Page):
             str2 = ', '.join(str(e) for e in [_ for _ in range(i+2, maxlen )])
             leftcol.append(str1)
             rightcol.append(str2)
-        print('LEFTCOL:', leftcol)
-        print('RIGHTCOL:', rightcol)
-        print('LENGHT LEFTCOL:', len(leftcol))
-        print('LENGHT FIELDS:', len(fields_to_show))
-        myform = NameForm(fields_to_show)
-        # return {'myform': myform}
-        # others = self.player.get_others_in_group()
-        # contribs_to_show = [p.contribution for p in others if p.isTLC]
-        # verbose_names = ['Participant {}'.format(p.id_in_group) for p in others if p.isTLC]
-        # # fields_to_show = self.get_form_fields()
-        # myform = NameForm(fields_to_show)
-        return {'abclist':zip(myform,leftcol,rightcol),}
+
+        return {'abclist':zip(smth,leftcol,rightcol),}
 # class DecisionsA(Page):
 #     pass
 # class DecisionsB(Page):
